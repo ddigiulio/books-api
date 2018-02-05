5b447f00bbb8cb7151c02e56092f62c251ebf36f
ddigiulio90 <ddigiulio90@gmail.com>
2018-02-05 16:01:39 -0500

    try

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 const { router: booksRouter } = require('./books')
 const { router: authorsRouter } = require('./authors')
 var cors = require('cors')
+const {CLIENT_ORIGIN} = require('./config');
 mongoose.Promise = global.Promise;
 
 const { PORT, DATABASE_URL } = require('./config');
@@ -19,16 +20,20 @@ const app = express();
 app.use(morgan('common'));
 //comment
 // CORS
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.send(204);
-  }
-  next();
-});
-// app.use(cors())
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+//   if (req.method === 'OPTIONS') {
+//     return res.send(204);
+//   }
+//   next();
+// });
+app.use(
+  cors({
+      origin: CLIENT_ORIGIN
+  })
+);
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
